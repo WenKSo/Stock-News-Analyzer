f94df46f5e06464d99bff7b553e6119a81a2d928
Wenkang Su <wenso.new@gmail.com>
2025-03-04 00:01:42 +0800

    AKSHARE Added

--- config/config.py
+++ config/config.py
@@ -1,5 +1,2 @@
 # Doubao-1.5-lite API 的访问地址和密钥（请根据实际情况替换）
 ARK_API_KEY = "eb66b444-6d15-4aec-8ba3-890fbbdf0a53"
-
-# Tushare 的 token（请在 tushare.pro 注册后获得）
-TUSHARE_TOKEN = "cf8358452d80af718113261e94f97f9b84cd02e3fd6c656dc4fe33cd"
